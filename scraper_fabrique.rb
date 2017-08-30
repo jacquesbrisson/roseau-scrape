cb2cbf7631348196e8d46cd3925627e7ef7791b9
jacquesbrisson <jacquesbrisson@gmail.com>
2017-08-30 05:37:44 -0400

    upd du 3008

--- scraper_fabrique.rb
+++ scraper_fabrique.rb
@@ -1,22 +1,35 @@
 require "rubygems"
 require "nokogiri"
 require "open-uri" ##si le fichier est sur Internet
-require "json"
+require "csv"
 require "pp"## A enlever
 
-## Va chercher fichier JSON sur disque (sert de référence)
-ref = File.read('reference.json')
-precedent = JSON.parse(ref)
-pp precedent
+resultat = Array.new
+precedent = Array.new
 
-## Va chercher data sur site cible et construit var JSON
+## Va chercher fichier CSV sur disque (sert de référence)
+data_file = './reference.csv'
+CSV.foreach(data_file, headers: true) do |row|
+  precedent << row.to_hash
+end
+##pp precedent
+
+## Va chercher data sur site cible et construit Array
 page = Nokogiri::HTML(open("http://www.lafabrique.fr/catalogue.php?mode=new"))
 
 page.css('html body div#global div#Centrale div.BlocListeLivre').each do |el|
-  puts '{ "Auteur:"' + el.at_css('div.BlocListeLivreAuteur.LienBlocListeLivre').text + '", '
-  puts '"Titre:"' + el.at_css('div.BlocListeLivreTitre.LienBlocListeLivre').text + '" },'
+  resultat << el.at_css('div.BlocListeLivreAuteur.LienBlocListeLivre').text
+  resultat << el.at_css('div.BlocListeLivreTitre.LienBlocListeLivre').text
+  ##pp el.at_css('div.BlocListeLivreAuteur.LienBlocListeLivre').text
+  ##pp el.at_css('div.BlocListeLivreTitre.LienBlocListeLivre').text
 end
-PEUT-ETRE EN STRING OU EN ARRAY ET ENSUITE ON Parse EN JSON
+##pp resultat
+
+if (precedent - resultat).empty?
+  pp "PAS DE CHANGEMENT"
+end
+
+##PEUT-ETRE EN STRING OU EN ARRAY ET ENSUITE ON Parse EN JSON
 
 ## Compare les deux VAR et conclut
 
